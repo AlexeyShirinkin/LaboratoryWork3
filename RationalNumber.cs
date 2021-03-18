a4dce61c3d57fc1251ae6255a515b06d7a024d84
Alexey <winderton228@yandex.ru>
2021-03-18 17:54:55 +0500

    Добавьте файлы проекта.

---
+++ RationalNumber.cs
@@ -0,0 +1,41 @@
+﻿using System.Numerics;
+
+namespace LaboratoryWork3
+{
+    public class RationalNumber
+    {
+        public BigInteger Numerator { get; }
+        public BigInteger Denominator { get; }
+
+        public RationalNumber(BigInteger numerator, BigInteger denominator)
+        {
+            Numerator = numerator;
+            Denominator = denominator;
+        }
+
+        public static RationalNumber operator +(RationalNumber number1, RationalNumber number2)
+        {
+            var leastCommonMultiple = LeastCommonMultiple(number1.Denominator, number2.Denominator);
+            var numerator = number1.Numerator * leastCommonMultiple / number1.Denominator
+                            + number2.Numerator * leastCommonMultiple / number2.Denominator;
+            return ReduceRationalNumber(new RationalNumber(numerator, leastCommonMultiple));
+        }
+
+        public static RationalNumber operator -(RationalNumber number1, RationalNumber number2)
+        {
+            var leastCommonMultiple = LeastCommonMultiple(number1.Denominator, number2.Denominator);
+            var numerator = number1.Numerator * leastCommonMultiple / number1.Denominator
+                            - number2.Numerator * leastCommonMultiple / number2.Denominator;
+            return ReduceRationalNumber(new RationalNumber(numerator, leastCommonMultiple));
+        }
+
+        public static BigInteger LeastCommonMultiple(BigInteger number1, BigInteger number2)
+            => number1 * number2 / BigInteger.GreatestCommonDivisor(number1, number2);
+
+        public static RationalNumber ReduceRationalNumber(RationalNumber number)
+        {
+            var greatestCommonDivisor = BigInteger.GreatestCommonDivisor(number.Numerator, number.Denominator);
+            return new RationalNumber(number.Numerator / greatestCommonDivisor, number.Denominator / greatestCommonDivisor);
+        }
+    }
+}
---
+++ RationalNumberTests.cs
@@ -0,0 +1,42 @@
+﻿using System.Numerics;
+using NUnit.Framework;
+
+namespace LaboratoryWork3
+{
+    internal class RationalNumberTests
+    {
+        [TestCase("4", "18", "2", "12", "7", "18", TestName = "WhenSmallNumbers")]
+        [TestCase("-4", "18", "-8", "12", "-8", "9", TestName = "WhenNegativeNumbersNumbers")]
+        [TestCase("4", "18", "-8", "12", "-4", "9", TestName = "WhenNegativeAndPositiveNumbersNumbers")]
+        [TestCase("2251954", "56591152", "6519851", "12651515", "198728254384331", "357981904197640", TestName = "WhenBigNumbers")]
+        public void CorrectAddition(string numerator1, string denominator1,
+            string numerator2, string denominator2,
+            string expectedNumerator, string expectedDenominator)
+        {
+            var number1 = new RationalNumber(BigInteger.Parse(numerator1), BigInteger.Parse(denominator1));
+            var number2 = new RationalNumber(BigInteger.Parse(numerator2), BigInteger.Parse(denominator2));
+
+            var actualNumber = number1 + number2;
+
+            Assert.AreEqual(BigInteger.Parse(expectedNumerator), actualNumber.Numerator);
+            Assert.AreEqual(BigInteger.Parse(expectedDenominator), actualNumber.Denominator);
+        }
+
+        [TestCase("4", "18", "2", "12", "1", "18", TestName = "WhenSmallNumbers")]
+        [TestCase("-4", "18", "-8", "12", "4", "9", TestName = "WhenNegativeNumbersNumbers")]
+        [TestCase("4", "18", "-8", "12", "8", "9", TestName = "WhenNegativeAndPositiveNumbersNumbers")]
+        [TestCase("2251954", "56591152", "6519851", "12651515", "-170237624574021", "357981904197640", TestName = "WhenBigNumbers")]
+        public void CorrectSubtraction(string numerator1, string denominator1,
+            string numerator2, string denominator2,
+            string expectedNumerator, string expectedDenominator)
+        {
+            var number1 = new RationalNumber(BigInteger.Parse(numerator1), BigInteger.Parse(denominator1));
+            var number2 = new RationalNumber(BigInteger.Parse(numerator2), BigInteger.Parse(denominator2));
+
+            var actualNumber = number1 - number2;
+
+            Assert.AreEqual(BigInteger.Parse(expectedNumerator), actualNumber.Numerator);
+            Assert.AreEqual(BigInteger.Parse(expectedDenominator), actualNumber.Denominator);
+        }
+    }
+}
