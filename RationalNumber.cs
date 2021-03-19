5d5f755897e9f0ed704c0fbb26d99cba4b555047
Alexey <winderton228@yandex.ru>
2021-03-19 14:49:57 +0500

    Arithmetic operations have been implemented

--- RationalNumber.cs
+++ RationalNumber.cs
@@ -9,6 +9,12 @@ namespace LaboratoryWork3
 
         public RationalNumber(BigInteger numerator, BigInteger denominator)
         {
+            if (denominator.Sign == -1)
+            {
+                numerator = BigInteger.Negate(numerator);
+                denominator = BigInteger.Negate(denominator);
+            }
+
             Numerator = numerator;
             Denominator = denominator;
         }
@@ -27,6 +33,22 @@ namespace LaboratoryWork3
             var numerator = number1.Numerator * leastCommonMultiple / number1.Denominator
                             - number2.Numerator * leastCommonMultiple / number2.Denominator;
             return ReduceRationalNumber(new RationalNumber(numerator, leastCommonMultiple));
+        }
+
+        public static RationalNumber operator *(RationalNumber number1, RationalNumber number2)
+        {
+            return ReduceRationalNumber(
+                new RationalNumber(
+                    number1.Numerator * number2.Numerator,
+                    number1.Denominator * number2.Denominator));
+        }
+
+        public static RationalNumber operator /(RationalNumber number1, RationalNumber number2)
+        {
+            return ReduceRationalNumber(
+                new RationalNumber(
+                    number1.Numerator * number2.Denominator,
+                    number1.Denominator * number2.Numerator));
         }
 
         public static BigInteger LeastCommonMultiple(BigInteger number1, BigInteger number2)
--- RationalNumberTests.cs
+++ RationalNumberTests.cs
@@ -6,8 +6,8 @@ namespace LaboratoryWork3
     internal class RationalNumberTests
     {
         [TestCase("4", "18", "2", "12", "7", "18", TestName = "WhenSmallNumbers")]
-        [TestCase("-4", "18", "-8", "12", "-8", "9", TestName = "WhenNegativeNumbersNumbers")]
-        [TestCase("4", "18", "-8", "12", "-4", "9", TestName = "WhenNegativeAndPositiveNumbersNumbers")]
+        [TestCase("-4", "18", "-8", "12", "-8", "9", TestName = "WhenNegativeNumbers")]
+        [TestCase("4", "18", "-8", "12", "-4", "9", TestName = "WhenNegativeAndPositiveNumber")]
         [TestCase("2251954", "56591152", "6519851", "12651515", "198728254384331", "357981904197640", TestName = "WhenBigNumbers")]
         public void CorrectAddition(string numerator1, string denominator1,
             string numerator2, string denominator2,
@@ -23,8 +23,8 @@ namespace LaboratoryWork3
         }
 
         [TestCase("4", "18", "2", "12", "1", "18", TestName = "WhenSmallNumbers")]
-        [TestCase("-4", "18", "-8", "12", "4", "9", TestName = "WhenNegativeNumbersNumbers")]
-        [TestCase("4", "18", "-8", "12", "8", "9", TestName = "WhenNegativeAndPositiveNumbersNumbers")]
+        [TestCase("-4", "18", "-8", "12", "4", "9", TestName = "WhenNegativeNumbers")]
+        [TestCase("4", "18", "-8", "12", "8", "9", TestName = "WhenNegativeAndPositiveNumbers")]
         [TestCase("2251954", "56591152", "6519851", "12651515", "-170237624574021", "357981904197640", TestName = "WhenBigNumbers")]
         public void CorrectSubtraction(string numerator1, string denominator1,
             string numerator2, string denominator2,
@@ -34,6 +34,40 @@ namespace LaboratoryWork3
             var number2 = new RationalNumber(BigInteger.Parse(numerator2), BigInteger.Parse(denominator2));
 
             var actualNumber = number1 - number2;
+
+            Assert.AreEqual(BigInteger.Parse(expectedNumerator), actualNumber.Numerator);
+            Assert.AreEqual(BigInteger.Parse(expectedDenominator), actualNumber.Denominator);
+        }
+
+        [TestCase("4", "18", "2", "12", "1", "27", TestName = "WhenSmallNumbers")]
+        [TestCase("-4", "18", "-8", "12", "4", "27", TestName = "WhenNegativeNumbers")]
+        [TestCase("4", "18", "-8", "12", "-4", "27", TestName = "WhenNegativeAndPositiveNumbers")]
+        [TestCase("2251954", "56591152", "6519851", "12651515", "7341202269427", "357981904197640", TestName = "WhenBigNumbers")]
+        public void CorrectMultiplication(string numerator1, string denominator1,
+            string numerator2, string denominator2,
+            string expectedNumerator, string expectedDenominator)
+        {
+            var number1 = new RationalNumber(BigInteger.Parse(numerator1), BigInteger.Parse(denominator1));
+            var number2 = new RationalNumber(BigInteger.Parse(numerator2), BigInteger.Parse(denominator2));
+
+            var actualNumber = number1 * number2;
+
+            Assert.AreEqual(BigInteger.Parse(expectedNumerator), actualNumber.Numerator);
+            Assert.AreEqual(BigInteger.Parse(expectedDenominator), actualNumber.Denominator);
+        }
+
+        [TestCase("4", "18", "2", "12", "4", "3", TestName = "WhenSmallNumbers")]
+        [TestCase("-4", "18", "-8", "12", "1", "3", TestName = "WhenNegativeNumbers")]
+        [TestCase("4", "18", "-8", "12", "-1", "3", TestName = "WhenNegativeAndPositiveNumbers")]
+        [TestCase("2251954", "56591152", "6519851", "12651515", "14245314905155", "184482939479176", TestName = "WhenBigNumbers")]
+        public void CorrectDivision(string numerator1, string denominator1,
+            string numerator2, string denominator2,
+            string expectedNumerator, string expectedDenominator)
+        {
+            var number1 = new RationalNumber(BigInteger.Parse(numerator1), BigInteger.Parse(denominator1));
+            var number2 = new RationalNumber(BigInteger.Parse(numerator2), BigInteger.Parse(denominator2));
+
+            var actualNumber = number1 / number2;
 
             Assert.AreEqual(BigInteger.Parse(expectedNumerator), actualNumber.Numerator);
             Assert.AreEqual(BigInteger.Parse(expectedDenominator), actualNumber.Denominator);
